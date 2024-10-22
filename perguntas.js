3c1e0fa627bf1d58f05eb787c05423d69b1a3670
laurinha1ACESA <144136185+laurinha1ACESA@users.noreply.github.com>
2024-10-22 10:39:25 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Biologia',
+    'Qual é o papel da Biologia?',
+    'Auxilia na prevenção e combate de doenças. Podemos também fazer previsões sobre o impacto da humanidade sobre si mesma e sobre as outras espécies do planeta.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Biologia',
+    'Quais as áreas de estudos da Biologia?',
+    'Genética, Botânica, Zoologia, Ecologia, Morfologia, Fisiologia, Bioquímica, Biofísica, Farmacologia, Imunologia, Microbiologia e Parasitologia.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Biologia',
+    'Qual é a base da Biologia?',
+    'A Biologia é a área de estudo da vida e de todos os tipos de seres vivos existentes, desde microscópicos, como bactérias, até os macroscópicos, como os animais, além das interações dos organismos com o ambiente e entre si, a reprodução e a evolução das espécies.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Biologia',
+    'Qual é a ordem correta para estudar biologia?',
+    'Após a biologia celular, é recomendado estudar a biologia molecular, que se aprofunda nos processos bioquímicos e genéticos que ocorrem nas células. Em seguida, pode-se avançar para a fisiologia, que explora o funcionamento dos organismos vivos e seus sistemas.'
 )
